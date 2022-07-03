7d18100d9dc10ccac138200576f5fd3f94a9bef1
sixtusiwuchukwu <sixtusiwuchukwu21@gmail.com>
2022-07-03 13:13:12 +0100

    first commi

--- src/utils/tokenGenerator.ts
+++
@@ -1,6 +0,0 @@
-import { User } from "../interface/userInterface";
-
-import jwt from "jsonwebtoken";
-export default async function getToken({ email, _id }: User) {
-  return jwt.sign({ email, _id }, process.env.SECRETKEY,{ expiresIn: 60 * 60 });
-}
